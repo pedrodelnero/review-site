337e4350ef69951fcdeedcd678e86ecc0731aad5
Pedro Del Nero <pedr.delnero@gmail.com>
2021-02-18 15:55:17 -0500

    heroku commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const uri = `'mongodb+srv://pedrod:${process.env.MONGODB_PW}@productreview.iwb5l.gcp.mongodb.net/test?retryWrites=true&w=majority'`;
 mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@productreview.iwb5l.gcp.mongodb.net/test?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(`Error: ${err}`));
