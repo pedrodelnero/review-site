74aa81281a70682cd92914163d53f2e56643a142
Pedro Del Nero <pedr.delnero@gmail.com>
2021-02-18 16:06:29 -0500

    heroku commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@productreview.iwb5l.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@productreview.iwb5l.gcp.mongodb.net/test?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useCreateIndex: true,
